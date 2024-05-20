6766b885e6afb1d39a6c104b0e6229b1d9421885
gmaldo <gmaldo@gmail.com>
2024-05-20 11:52:06 -0300

    Counter for stock

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
+import "./ItemDetail.css"
+import Counter from '../Counter/Counter';
 
 const ItemDetail = ({product}) => {
-  return (
-    <div className="container mt-5">
-        <div className="row">
-            <div className="col-md-6">
-                <img src={product.image} alt={product.title} className="img-fluid" />
+    const stock = 10
+    return (
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-md-6">
+                    <img src={product.image} alt={product.title} className="img-fluid" />
+                </div>
+                <div className="col-md-6">
+                    <h2>{product.title}</h2>
+                    <p className="text-muted">{product.description}</p>
+                    <p className="price">Precio: ${product.price}</p>
+                    <Counter max={stock} />
+                    <button className="btn btn-primary">Añadir al carrito</button>
+                </div>
             </div>
-            <div className="col-md-6">
-                <h2>{product.title}</h2>
-                <p className="text-muted">{product.description}</p>
-                <p className="price">Precio: ${product.price}</p>
-                <button className="btn btn-primary">Añadir al carrito</button>
-            </div>
-        </div>
     </div>
 );
 }
