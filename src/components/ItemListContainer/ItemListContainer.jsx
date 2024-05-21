f50ac1837ced1d04a67765f107e576afb278019c
gmaldo <gmaldo@gmail.com>
2024-05-21 11:00:33 -0300

    Update ItemListCointainer now filters by categorie
    Using useParam() now gets category and filter

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,40 +2,41 @@ import {useState,useEffect} from 'react'
 import './ItemListContainer.css'
 import Loader from '../Loader/Loader'
 import ItemList from '../ItemList/ItemList'
+import { useParams } from 'react-router-dom'
 const ItemListContainer = ({messaje}) => {
   
-  const [productos, setProductos] = useState([])
-  
-  useEffect(() => {
+  const [products, setProducts] = useState([])
+  const {categoryid}  = useParams()
 
+  useEffect(() => {
     const fetchData = async () => {
-      
       try{
-       // const response = await fetch ("./data/productos.json")
-       const response = await fetch ("https://fakestoreapi.com/products")
-       const data = await response.json()
-       setProductos(data)
+        // const response = await fetch ("./data/productos.json")
+        const response = await fetch ("https://fakestoreapi.com/products")
+        const data = await response.json()
+        categoryid === undefined ?
+            setProducts(data) :
+            setProducts(data.filter(product => product.category === categoryid));
       }catch(error){
         console.log("Error al obtener los productos", error)
       }
-
     } 
 
     fetchData()
 
-  }, [])
+  }, [categoryid])
 
   return (
     <div className='itemListContainer'>
         <h2>Contenedor Lista de Items</h2>
         <p>{messaje}</p>
-        {productos.length == 0 ? 
+        {products.length == 0 ? 
 
           <Loader/>
 
           :
 
-          <ItemList productos={productos}/>
+          <ItemList productos={products}/>
 
         }
     </div>
