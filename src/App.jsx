93d925c7287a0a081978e37878b9ef7364ad5bf0
gmaldo <gmaldo@gmail.com>
2024-06-25 16:18:51 -0300

    EntregaFinal+Maldonado
    removed unused components and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
-import { useState } from 'react'
 import './App.css'
-import NavBar from './components/Navbar/NavBar'
-import ItemListContainer from './components/ItemListContainer/ItemListContainer'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
-import Cart from './components/Cart/Cart'
 import AppRouter from './router/AppRouter'
 import CartProvider from './context/CartProvider'
 import 'bootstrap/dist/css/bootstrap.min.css';
